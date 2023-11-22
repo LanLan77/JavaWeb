ea5e176227f5cb04ad792b4884ec253e792161c2
LanLan77 <775172380@qq.com>
2023-11-23 00:55:58 +1030

    Day03-Element-快速入门

--- Day03-Vue-Element/vue/vue-project/src/main.js
+++ Day03-Vue-Element/vue/vue-project/src/main.js
@@ -6,8 +6,9 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
