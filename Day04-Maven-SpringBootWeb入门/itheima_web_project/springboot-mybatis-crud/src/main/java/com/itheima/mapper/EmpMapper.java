8a4c7575b859c59d23231db2942a3537c68dc77a
LanLan77 <775172380@qq.com>
2024-04-22 20:03:28 +0930

    Day09-04. Mybatis-基础操作-新增

--- Day04-Maven-SpringBootWeb入门/itheima_web_project/springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
+++ Day04-Maven-SpringBootWeb入门/itheima_web_project/springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -1,5 +1,6 @@
 package com.itheima.mapper;
 
+import com.itheima.pojo.Emp;
 import org.apache.ibatis.annotations.*;
 
 @Mapper
@@ -7,4 +8,9 @@ public interface EmpMapper {
     // 根据ID删除数据
     @Delete("delete from emp where id= #{id }")
     public void delete(Integer id);
+
+    // 新增员工
+    @Insert("INSERT INTO emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time) " +
+            "VALUES (#{username}, #{name}, #{gender}, #{image}, #{job}, #{entrydate}, #{deptId}, now(), now())")
+    public void insert(Emp emp);
 }
--- Day04-Maven-SpringBootWeb入门/itheima_web_project/springboot-mybatis-crud/src/test/java/com/itheima/SpringbootMybatisCrudApplicationTests.java
+++ Day04-Maven-SpringBootWeb入门/itheima_web_project/springboot-mybatis-crud/src/test/java/com/itheima/SpringbootMybatisCrudApplicationTests.java
@@ -1,9 +1,13 @@
 package com.itheima;
 
 import com.itheima.mapper.EmpMapper;
+import com.itheima.pojo.Emp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SpringbootMybatisCrudApplicationTests {
@@ -14,6 +18,23 @@ class SpringbootMybatisCrudApplicationTests {
     @Test
     public void testDelete(){
         empMapper.delete(17);
+    }
+
+    @Test
+    public void testInsert(){
+        Emp emp = new Emp();
+        emp.setUsername("Tom");
+        emp.setName("汤姆");
+        emp.setImage("1.jpg");
+        emp.setGender((short)1);
+        emp.setJob((short)2);
+        emp.setEntrydate(LocalDate.of(2000,1,1));
+        emp.setCreateTime(LocalDateTime.now());
+        emp.setUpdateTime(LocalDateTime.now());
+        emp.setDeptId(1);
+
+        // 执行新增员工操作
+        empMapper.insert(emp);
     }
 
 }
