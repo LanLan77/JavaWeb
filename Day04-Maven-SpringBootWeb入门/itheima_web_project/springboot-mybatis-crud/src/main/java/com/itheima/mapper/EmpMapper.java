983213a8c02ec9d9cb56052d7a2a07fc6bf42a97
LanLan77 <775172380@qq.com>
2024-05-09 12:11:48 +0930

    Day09-09. Mybatis-XML映射文件

--- Day04-Maven-SpringBootWeb入门/itheima_web_project/springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
+++ Day04-Maven-SpringBootWeb入门/itheima_web_project/springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -3,15 +3,22 @@ package com.itheima.mapper;
 import com.itheima.pojo.Emp;
 import org.apache.ibatis.annotations.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Mapper
 public interface EmpMapper {
-    // 根据ID删除数据
-    @Delete("delete from emp where id= #{id}")
-    public void delete(Integer id);
 
-    // 新增员工
-    @Insert("INSERT INTO emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time) " +
-            "VALUES (#{username}, #{name}, #{gender}, #{image}, #{job}, #{entrydate}, #{deptId}, now(), now())")
+    //根据ID删除数据
+    @Delete("delete from emp where id = #{id}")
+    public void delete(Integer id);
+    //public int delete(Integer id);
+
+
+    //新增员工
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("insert into emp(username, name, gender, image, job, entrydate, dept_id, create_time, update_time)" +
+            " values (#{username},#{name},#{gender},#{image},#{job},#{entrydate},#{deptId},#{createTime},#{updateTime})")
     public void insert(Emp emp);
 
 
@@ -42,9 +49,25 @@ public interface EmpMapper {
 
 
 
+    //条件查询员工
+    //方式一
+    //@Select("select * from emp where name like '%${name}%' and gender = #{gender} and " +
+    //        "entrydate between #{begin} and #{end} order by update_time desc ")
+    //public List<Emp> list(String name, Short gender, LocalDate begin , LocalDate end);
+
+    //方式二
+//    @Select("select * from emp where name like concat('%',#{name},'%') and gender = #{gender} and " +
+//            "entrydate between #{begin} and #{end} order by update_time desc ")
+//    public List<Emp> list(String name, Short gender, LocalDate begin , LocalDate end);
+
+    //动态条件查询
+    public List<Emp> list(String name, Short gender, LocalDate begin , LocalDate end);
 
 
+    //动态更新员工
+    public void update2(Emp emp);
 
 
-
+    //批量删除员工
+    public void deleteByIds(List<Integer> ids);
 }
